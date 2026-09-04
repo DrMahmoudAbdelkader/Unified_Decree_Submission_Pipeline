b1d1cceb65d9c5574060cf6f16f40de46ddade40
DrMahmoudAbdelkader <bluepharmacy123@gmail.com>
2026-09-04 18:00:52 +0300

    Change R2_BUCKET_NAME to a constant value
    Set R2_BUCKET_NAME to a fixed value instead of using an environment variable.

--- r2_client.py
+++ r2_client.py
@@ -39,7 +39,7 @@ log = logging.getLogger("r2_client")
 R2_ACCOUNT_ID = os.environ.get("R2_ACCOUNT_ID", "")
 R2_ACCESS_KEY_ID = os.environ.get("R2_ACCESS_KEY_ID", "")
 R2_SECRET_ACCESS_KEY = os.environ.get("R2_SECRET_ACCESS_KEY", "")
-R2_BUCKET_NAME = os.environ.get("R2_BUCKET_NAME", "cleaned-pdfs-files")
+R2_BUCKET_NAME = "cleaned-pdfs-files"
 
 _client = None
 
